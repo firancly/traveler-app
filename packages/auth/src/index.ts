c2db86022156160eceaf9a4c37eb8b0e46732021
FiranclyWin <faizfirdavs@gmail.com>
2026-09-08 23:39:11 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,38 @@
+import { expo } from "@better-auth/expo";
+import { createPrismaClient } from "@traveler-app/db";
+import { env } from "@traveler-app/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export function createAuth() {
+  const prisma = createPrismaClient();
+
+  return betterAuth({
+    database: prismaAdapter(prisma, {
+      provider: "postgresql",
+    }),
+
+    trustedOrigins: [
+      env.CORS_ORIGIN,
+
+      "traveler-app://",
+      "exp://",
+      "http://localhost:8081",
+    ],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
