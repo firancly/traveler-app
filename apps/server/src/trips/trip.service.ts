e2a02242d8bb54ccda53f5f4d2ad9dd8d18b1fb6
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-09-18 07:52:23 +0530

    feat:added reorder trips functionality

--- apps/server/src/trips/trip.service.ts
+++ apps/server/src/trips/trip.service.ts
@@ -1,159 +1,225 @@
-import prisma from "@traveler-app/db"
+import prisma from "@traveler-app/db";
 
 export async function tripService({
-    userId,
-    data, } : {
-        userId : string,
-        data : {
-        name: string,
-        description?: string,
-        startDate?: Date,
-        endDate?: Date,
-        }
-    }
-) {
-    const trip = await prisma.trip.create({
-        data : {
-            ownerId : userId,
-            name : data.name,
-            description : data.description,
-            startDate : data.startDate ? new Date(data.startDate) : undefined,
-            endDate : data.endDate ? new Date(data.endDate) : undefined,
+  userId,
+  data,
+}: {
+  userId: string;
+  data: {
+    name: string;
+    description?: string;
+    startDate?: Date;
+    endDate?: Date;
+  };
+}) {
+  const lastTripMember = await prisma.tripMember.findFirst({
+    where: {
+      userId,
+    },
+    orderBy: {
+      position: "desc",
+    },
+    select: {
+      position: true,
+    },
+  });
 
-            members : {
-                create : {
-                    userId,
-                    role : "owner",
-                }
-            }
-        }
-    })
+  const position = lastTripMember ? lastTripMember.position + 1 : 0;
 
-    return trip;
+  const trip = await prisma.trip.create({
+    data: {
+      ownerId: userId,
+      name: data.name,
+      description: data.description,
+      startDate: data.startDate ? new Date(data.startDate) : undefined,
+      endDate: data.endDate ? new Date(data.endDate) : undefined,
+
+      members: {
+        create: {
+          userId,
+          role: "owner",
+          position,
+        },
+      },
+    },
+  });
+
+  return trip;
 }
 
-export async function getTripsService({
-    userId,
-} : {
-    userId : string
-}){
-    const trips = await prisma.trip.findMany({
-        where : {
-            OR : [
-                {
-                    ownerId : userId,
-                },
-                {
-                    members : {
-                        some : {
-                            userId,
-                        },
-                    },
-                },
-            ],
+export async function getTripsService({ userId }: { userId: string }) {
+  const trips = await prisma.trip.findMany({
+    where: {
+      OR: [
+        {
+          ownerId: userId,
         },
-        orderBy : {
-            updatedAt : 'desc'
-        }
-    })
-    return trips;
+        {
+          members: {
+            some: {
+              userId,
+            },
+          },
+        },
+      ],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
+  return trips;
 }
 
 export async function getTripService({
-    userId, tripId,
-} : {
-    userId : string,
-    tripId : string,
-}){
-    const trip = await prisma.trip.findFirst({
-        where : {
-            id : tripId,
-            OR : [
-                {
-                    ownerId : userId,
-                },
-                {
-                    members : {
-                        some : {
-                            userId,
-                        },
-                    },
-                },
-            ],
+  userId,
+  tripId,
+}: {
+  userId: string;
+  tripId: string;
+}) {
+  const trip = await prisma.trip.findFirst({
+    where: {
+      id: tripId,
+      OR: [
+        {
+          ownerId: userId,
         },
-    });
-    return trip;
+        {
+          members: {
+            some: {
+              userId,
+            },
+          },
+        },
+      ],
+    },
+  });
+  return trip;
 }
 
 export async function updateTripService({
-    userId,
-    tripId,
-    data,
-} : {
-    userId : string;
-    tripId : string;
-    data : {
-        name? : string;
-        description? : string | null;
-        startDate? : string | null;
-        endDate? : string | null;
-    }
-}  
-) {
-    const trip = await prisma.trip.findFirst({
-        where : {
-            id : tripId,
-            OR : [
-                {
-                    ownerId : userId,
-                },
-                {
-                    members : {
-                        some : {
-                            userId,
-                            role : "editor",
-                        },
-                    },
-                },
-            ],
+  userId,
+  tripId,
+  data,
+}: {
+  userId: string;
+  tripId: string;
+  data: {
+    name?: string;
+    description?: string | null;
+    startDate?: string | null;
+    endDate?: string | null;
+  };
+}) {
+  const trip = await prisma.trip.findFirst({
+    where: {
+      id: tripId,
+      OR: [
+        {
+          ownerId: userId,
         },
-    });
-
-    if(!trip){
-        return null;
-    }
-    const updatedTrip = await prisma.trip.update({
-        where : {
-            id : tripId,
+        {
+          members: {
+            some: {
+              userId,
+              role: "editor",
+            },
+          },
         },
-        data : {
-            name : data.name,
-            description : data.description,
-            startDate : data.startDate ? new Date(data.startDate) : data.startDate === null ? null : undefined,
-            endDate : data.endDate ? new Date(data.endDate) : data.endDate === null ? null : undefined,
-        }
-    })
+      ],
+    },
+  });
 
-    return updatedTrip;
+  if (!trip) {
+    return null;
+  }
+  const updatedTrip = await prisma.trip.update({
+    where: {
+      id: tripId,
+    },
+    data: {
+      name: data.name,
+      description: data.description,
+      startDate: data.startDate
+        ? new Date(data.startDate)
+        : data.startDate === null
+        ? null
+        : undefined,
+      endDate: data.endDate
+        ? new Date(data.endDate)
+        : data.endDate === null
+        ? null
+        : undefined,
+    },
+  });
+
+  return updatedTrip;
 }
 
+export async function deleteTripService({
+  userId,
+  tripId,
+}: {
+  userId: string;
+  tripId: string;
+}) {
+  const trip = await prisma.trip.findFirst({
+    where: {
+      id: tripId,
+      ownerId: userId,
+    },
+  });
 
-export async function deleteTripService({userId, tripId} : {userId : string, tripId : string}) {
-    const trip = await prisma.trip.findFirst({
-        where : {
-            id : tripId,
-            ownerId : userId,
-        },
-    });
+  if (!trip) {
+    return null;
+  }
+  const deletedTrip = await prisma.trip.delete({
+    where: {
+      id: tripId,
+    },
+  });
 
-    if(!trip){
-        return null;
-    }
-    const deletedTrip = await prisma.trip.delete({
-        where : {
-            id : tripId,
-        },
-    });
+  return deletedTrip;
+}
 
-    return deletedTrip;
+export async function reorderTripsService({userId, tripIds} : {
+	userId : string;
+	tripIds : string[],
+}) {
+	const memberships = await prisma.tripMember.findMany({
+		where : {
+			userId,
+			tripId : {
+				in : tripIds,
+			},
+		},
+		select : {
+			tripId : true,
+		},
+	});
+
+	const userTripIds = new Set(
+		memberships.map((membership)=> membership.tripId),
+	);
+
+	if(userTripIds.size !== tripIds.length) {
+		throw new Error("Invalid trip list");
+	}
+
+	await prisma.$transaction(
+		tripIds.map((tripId, index)=>
+		prisma.tripMember.update({
+			where : {
+				tripId_userId : {
+					tripId,
+					userId,
+				},
+			},
+			data : {
+				position : index,
+			}
+		}))
+	)
+
+	return true;
 }
