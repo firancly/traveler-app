319b66be26bd8a5b051945a61f7830425c720e5c
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-09-26 16:25:03 +0530

    feat: Added get All trip members

--- apps/server/src/trips/trip-member/trip-member.service.ts
+++ apps/server/src/trips/trip-member/trip-member.service.ts
@@ -1,33 +1,87 @@
 import prisma from "@traveler-app/db";
 
-export async function updateTripMemberRoleService({userId, tripId, targetUserId, role} : {userId : string, tripId : string, targetUserId : string, role : "editor" | "viewer"}) {
-	const ownerMemberShipRole = await prisma.tripMember.findUnique({
-		where : {
-			tripId_userId : {
-				tripId, 
-				userId
-			},
-		},
-		select : {
-			role : true,
-		}
-	})
+export async function updateTripMemberRoleService({
+  userId,
+  tripId,
+  targetUserId,
+  role,
+}: {
+  userId: string;
+  tripId: string;
+  targetUserId: string;
+  role: "editor" | "viewer";
+}) {
+  const ownerMemberShipRole = await prisma.tripMember.findUnique({
+    where: {
+      tripId_userId: {
+        tripId,
+        userId,
+      },
+    },
+    select: {
+      role: true,
+    },
+  });
 
-	if(ownerMemberShipRole?.role !== "owner"){
-		return null
-	}
+  if (ownerMemberShipRole?.role !== "owner") {
+    return null;
+  }
 
-	const updateMember = await prisma.tripMember.update({
-		where : {
-			tripId_userId : {
-				tripId,
-				userId: targetUserId
-			},
-		},
-		data : {
-			role,
-		},
-	});
+  const updateMember = await prisma.tripMember.update({
+    where: {
+      tripId_userId: {
+        tripId,
+        userId: targetUserId,
+      },
+    },
+    data: {
+      role,
+    },
+  });
 
-	return updateMember
+  return updateMember;
+}
+
+export async function getTripMembersService({
+  userId,
+  tripId,
+}: {
+  userId: string;
+  tripId: string;
+}) {
+  const member = await prisma.tripMember.findUnique({
+    where: {
+      tripId_userId: {
+        tripId,
+        userId,
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!member) {
+    return null;
+  }
+
+  const members = await prisma.tripMember.findMany({
+    where: {
+      tripId,
+    },
+    select: {
+      id: true,
+      role: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          image: true,
+        },
+      },
+    },
+  });
+
+  return members;
 }
